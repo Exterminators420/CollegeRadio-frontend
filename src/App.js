1fe7c4f8e5d44e4399587f3ca56d69543f4fdd7c
Rohit <rohit.uph2017@iitr.ac.in>
2018-09-29 04:37:22 +0530

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import YTSearch from './Components/ytsearch';
+import Navbar from "./Components/navbar"
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
+
+        <Navbar />
+
         <YTSearch/>
 
       </div>
