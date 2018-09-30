26a9e048e82491a8a034ebfae8007d9b16ae2a71
Rohit <rohit.uph2017@iitr.ac.in>
2018-09-30 07:31:08 +0530

    added homepage

---
+++ src/Components/home.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+
+export default class Home extends Component {
+
+    render() {
+
+        return (
+            <div className = "home">
+            
+            
+            <center>
+                <h1>
+                    Music <br></br>For The People. By the People. With the People.
+                </h1>
+                
+                <a href = "/channel/home">
+                    <button>
+                    College Radio
+                    </button>
+                </a>
+            </center>
+            
+            
+            
+            </div>
+
+
+
+
+        );
+
+    }
+
+
+}
+
